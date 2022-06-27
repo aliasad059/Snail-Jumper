9845d870c32f60c302697a639210c1478e23782f
a.asad <aliasad059@gmail.com>
2022-06-28 00:18:35 +0430

    feat: complete population generation and selection

--- evolution.py
+++ evolution.py
@@ -1,5 +1,6 @@
 import copy
-
+import random
+import numpy as np
 from player import Player
 
 
@@ -7,22 +8,52 @@ class Evolution:
     def __init__(self):
         self.game_mode = "Neuroevolution"
 
-    def next_population_selection(self, players, num_players):
+    def next_population_selection(self, players, num_players, policy='top-k', save_learning_info=False):
         """
         Gets list of previous and current players (μ + λ) and returns num_players number of players based on their
         fitness value.
 
         :param players: list of players in the previous generation
         :param num_players: number of players that we return
+        :param policy: policy for selection of players
+        :param save_learning_info: plot learning curve of the players
         """
-        # TODO (Implement top-k algorithm here)
-        # TODO (Additional: Implement roulette wheel here)
-        # TODO (Additional: Implement SUS here)
+        next_players = []
+        if policy == 'top-k':
+            players = sorted(players, key=lambda x: x.fitness, reverse=True)
+            next_players = players[:num_players]
+        elif policy == 'roulette':
+            total_fitness = sum([player.fitness for player in players])
+            for _ in range(num_players):
+                r = random.random() * total_fitness
+                for player in players:
+                    r -= player.fitness
+                    if r <= 0:
+                        next_players.append(player)
+                        break
+            pass
+        elif policy == 'tournament':
+            for _ in range(num_players):
+                players = random.sample(players, 2)
+                if players[0].fitness > players[1].fitness:
+                    next_players.append(players[0])
+                else:
+                    next_players.append(players[1])
+            pass
+        elif policy == 'sus':
+            pass
 
-        # TODO (Additional: Learning curve)
-        return players[: num_players]
+        if save_learning_info:
+            # write learning info to a file
+            sorted_next_players = sorted(next_players, key=lambda x: x.fitness, reverse=True)
+            with open('learning_info.txt', 'a') as f:
+                best_fit = sorted_next_players[0].fitness
+                worst_fit = sorted_next_players[-1].fitness
+                average_fit = sum([player.fitness for player in sorted_next_players]) / len(sorted_next_players)
+                f.write(f'{best_fit},{worst_fit},{average_fit}\n')
+        return next_players
 
-    def generate_new_population(self, num_players, prev_players=None):
+    def generate_new_population(self, num_players, prev_players=None, policy='top-k'):
         """
         Gets survivors and returns a list containing num_players number of children.
 
@@ -34,9 +65,16 @@ class Evolution:
         if first_generation:
             return [Player(self.game_mode) for _ in range(num_players)]
         else:
-            # TODO ( Parent selection and child generation )
-            new_players = prev_players  # DELETE THIS AFTER YOUR IMPLEMENTATION
-            return new_players
+            parents = prev_players
+            children = []
+            for _ in range(num_players):
+                p1 = random.choice(parents)
+                p2 = random.choice(parents)
+                child = self.cross_over(p1, p2)
+                if random.random() < 0.2:
+                    child = self.mutate(child)
+                children.append(child)
+            return children
 
     def clone_player(self, player):
         """
@@ -46,3 +84,24 @@ class Evolution:
         new_player.nn = copy.deepcopy(player.nn)
         new_player.fitness = player.fitness
         return new_player
+
+    def cross_over(self, p1, p2):
+        """
+        Gets two players as an input and produces a child by crossing over the weights and biases of the two players.
+        """
+        child = self.clone_player(p1)
+        for layer in p1.nn.weights.keys():
+            if layer % 2 == 0:
+                child.nn.weights[layer] = p2.nn.weights[layer]
+                child.nn.biases[layer] = p2.nn.biases[layer]
+        child.fitness = (child.fitness + p2.fitness) / 2
+        return child
+
+    def mutate(self, player):
+        """
+        Gets a player as an input and produces a mutated player.
+        """
+        for layer in player.nn.weights.keys():
+            player.nn.weights[layer] += np.random.normal(0, 0.1, player.nn.weights[layer].shape)
+            player.nn.biases[layer] += np.random.normal(0, 0.1, player.nn.biases[layer].shape)
+        return player
